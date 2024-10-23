f09fad41bfeb9b46fbc0834b258b829ec0c2012f
SHRIHARI-25 <shrihari2504@gmail.com>
2024-10-23 18:16:10 +0530

    message

---
+++ ang.ts
@@ -0,0 +1,15 @@
+demo-project/
+│
+├── src/
+│   ├── app/
+│   │   ├── hello/
+│   │   │   ├── hello.component.css
+│   │   │   ├── hello.component.html
+│   │   │   ├── hello.component.ts
+│   │   │   └── hello.component.spec.ts
+│   │   ├── app.component.css
+│   │   ├── app.component.html
+│   │   ├── app.component.ts
+│   │   └── app.module.ts
+│   ├── index.html
+│   └── main.ts
